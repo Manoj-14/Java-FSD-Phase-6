6cd7c6bdf3bf579e70cef926c58fc3bda55a77c6
Manoj M <manumadhu1425@gmail.com>
2023-04-28 11:04:51 +0530

    Chalk module

---
+++ NodeDemos/N05Chalk.js
@@ -0,0 +1,6 @@
+import chalk from "chalk";
+
+console.log(chalk.blue("Hello world!"));
+console.log(chalk.blue.bold("Hello world!"));
+console.log(chalk.red.bold("error"));
+console.log(chalk.blue.inverse("Hello world!"));
