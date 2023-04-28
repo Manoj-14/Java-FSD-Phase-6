6f62887c2d0c7de02eddb444220b6bd98c1d8de8
Manoj M <manumadhu1425@gmail.com>
2023-04-28 10:20:25 +0530

    Node intro and Module exports

---
+++ NodeDemos/N01Intro.js
@@ -0,0 +1,6 @@
+const utils = require("./N02Utils.js");
+const { name, add } = require("./N02Utils.js");
+// console.log(utils.name);
+// console.log(utils.add(2, 3));
+console.log(name);
+console.log(add(1, 3));
---
+++ NodeDemos/N02Utils.js
@@ -0,0 +1,11 @@
+// console.log("Utils");
+
+const name = "Tom";
+
+// module.exports = name;
+
+const add = function (a, b) {
+  return a + b;
+};
+
+module.exports = { name, add };
